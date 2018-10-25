07e1469664eeb486bfcdf50a5c656c16f296123f
yoichi kobayashi <info@tplh.net>
2018-10-25 11:55:19 +0900

    added Capsule script to loop the capsule position.

---
+++ hello-world/Assets/CapsuleScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Transform myTransform = this.transform;
+
+		Vector3 pos = myTransform.position;
+		if (pos.y < -10) {
+			pos.x = 0;
+			pos.y = 10;
+			pos.z = 0;
+			myTransform.position = pos;
+		}
+	}
+}
