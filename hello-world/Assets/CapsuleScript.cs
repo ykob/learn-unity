7ecd515a7887205d6a287fd3dd4d238fdc0d6688
yoichi kobayashi <info@tplh.net>
2018-11-19 01:06:07 +0900

    added text objects to view the falling capsule count.

--- hello-world/Assets/CapsuleScript.cs
+++ hello-world/Assets/CapsuleScript.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CapsuleScript : MonoBehaviour {
+  private int number;
 
 	// Use this for initialization
 	void Start () {
+    number = 0;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		Transform myTransform = this.transform;
 		Rigidbody myRigidbody = GetComponent<Rigidbody>();
+    var numberText = GameObject.Find("NumberText").GetComponent<Text>();
 
 		Vector3 pos = myTransform.position;
 		if (pos.y < -20) {
@@ -20,6 +24,9 @@ public class CapsuleScript : MonoBehaviour {
 			pos.z = 0;
 			myTransform.position = pos;
 			myRigidbody.velocity = Vector3.zero;
+
+      number = number + 1;
+      numberText.text = number.ToString();
 		}
 	}
 }
