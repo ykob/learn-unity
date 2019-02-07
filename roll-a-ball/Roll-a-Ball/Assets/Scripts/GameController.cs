32c425ffc5b3325c7b150bd75aa3883601e34a53
yoichi kobayashi <info@tplh.net>
2019-02-07 18:06:50 +0900

    Added the processing to set winnerLabel to active.

--- roll-a-ball/Roll-a-Ball/Assets/Scripts/GameController.cs
+++ roll-a-ball/Roll-a-Ball/Assets/Scripts/GameController.cs
@@ -3,8 +3,15 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 	public UnityEngine.UI.Text scoreLabel;
+	public GameObject winnerLabelObject;
+
 	public void Update() {
 		int count = GameObject.FindGameObjectsWithTag("Item").Length;
 		scoreLabel.text = count.ToString();
+
+		if (count == 0) {
+			// クリア時の処理
+			winnerLabelObject.SetActive(true);
+		}
 	}
 }
