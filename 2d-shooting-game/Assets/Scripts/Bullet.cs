a9f077ac2bdd7d57726e8b40d933d790506094ea
yoichi kobayashi <info@tplh.net>
2019-02-28 00:05:54 +0900

    make the Bullet script.

---
+++ 2d-shooting-game/Assets/Scripts/Bullet.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	public int speed = 10;
+
+	void Start() {
+		GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+	}
+}
--- 2d-shooting-game/Assets/Scripts/Player.cs
+++ 2d-shooting-game/Assets/Scripts/Player.cs
@@ -5,8 +5,8 @@ public class Player : MonoBehaviour {
 	public float speed = 5;
 
 	void Update() {
-		float x = Input.GetAxisRaw('Horizontal');
-		float y = Input.GetAxisRaw('Vertical');
+		float x = Input.GetAxisRaw("Horizontal");
+		float y = Input.GetAxisRaw("Vertical");
 		Vector2 direction = new Vector2(x, y).normalized;
 		GetComponent<Rigidbody2D>().velocity = direction * speed;
 	}
