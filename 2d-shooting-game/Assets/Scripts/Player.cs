51a6e87be3968ef4777a3622170c7e88617f99ed
yoichi kobayashi <info@tplh.net>
2019-02-27 23:38:12 +0900

    make the Player script.

---
+++ 2d-shooting-game/Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+﻿using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float speed = 5;
+
+	void Update() {
+		float x = Input.GetAxisRaw('Horizontal');
+		float y = Input.GetAxisRaw('Vertical');
+		Vector2 direction = new Vector2(x, y).normalized;
+		GetComponent<Rigidbody2D>().velocity = direction * speed;
+	}
+}
