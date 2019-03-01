3ecc9e7503258d12eef886df99a17b1cb072f6dd
yoichi kobayashi <info@tplh.net>
2019-03-02 00:12:42 +0900

    added the shooting animation.

--- 2d-shooting-game/Assets/Scripts/Player.cs
+++ 2d-shooting-game/Assets/Scripts/Player.cs
@@ -2,7 +2,21 @@ using UnityEngine;
 ﻿using System.Collections;
 
 public class Player : MonoBehaviour {
+	// 移動スピード
 	public float speed = 5;
+
+	// PlayerBulletプレハブ
+	public GameObject bullet;
+
+	// Startメソッドをコルーチンとして呼び出す
+	IEnumerator Start() {
+		while(true) {
+			// 弾をプレイヤーと同じ位置/確度で作成
+			Instantiate (bullet, transform.position, transform.rotation);
+			// 0.05秒待つ
+			yield return new WaitForSeconds(0.05f);
+		}
+	}
 
 	void Update() {
 		float x = Input.GetAxisRaw("Horizontal");
