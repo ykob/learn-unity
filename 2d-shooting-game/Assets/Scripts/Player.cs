b1773d54e0ff01cc146be2073a258b60581eb827
yoichi kobayashi <info@tplh.net>
2019-03-02 17:42:03 +0900

    make a new Script Spaceship.

--- 2d-shooting-game/Assets/Scripts/Player.cs
+++ 2d-shooting-game/Assets/Scripts/Player.cs
@@ -2,26 +2,28 @@ using UnityEngine;
 ﻿using System.Collections;
 
 public class Player : MonoBehaviour {
-	// 移動スピード
-	public float speed = 5;
-
-	// PlayerBulletプレハブ
-	public GameObject bullet;
+	Spaceship spaceship; // Spaceshipコンポーネント
 
 	// Startメソッドをコルーチンとして呼び出す
 	IEnumerator Start() {
+		// Spaceshipコンポーネントを取得
+		spaceship = GetComponent<Spaceship>();
+
 		while(true) {
 			// 弾をプレイヤーと同じ位置/確度で作成
-			Instantiate (bullet, transform.position, transform.rotation);
-			// 0.05秒待つ
-			yield return new WaitForSeconds(0.05f);
+			spaceship.Shot(transform);
+			// shotDelay秒待つ
+			yield return new WaitForSeconds(spaceship.shotDelay);
 		}
 	}
 
 	void Update() {
-		float x = Input.GetAxisRaw("Horizontal");
-		float y = Input.GetAxisRaw("Vertical");
+		float x = Input.GetAxisRaw("Horizontal"); // 右・左
+		float y = Input.GetAxisRaw("Vertical"); // 上・下
+
+		// 移動する向きを求める
 		Vector2 direction = new Vector2(x, y).normalized;
-		GetComponent<Rigidbody2D>().velocity = direction * speed;
+		// 移動
+		spaceship.Move(direction);
 	}
 }
---
+++ 2d-shooting-game/Assets/Scripts/Spaceship.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Spaceship : MonoBehaviour {
+	public float speed; // 移動スピード
+	public float shotDelay; // 弾を打つ間隔
+	public GameObject bullet; // 弾のPrefab
+
+	// 弾の作成
+	public void Shot(Transform origin) {
+		Instantiate(bullet, origin.position, origin.rotation);
+	}
+	// 機体の移動
+	public void Move(Vector2 direction) {
+		GetComponent<Rigidbody2D>().velocity = direction * speed;
+	}
+}
